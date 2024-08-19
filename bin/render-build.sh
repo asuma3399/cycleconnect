c22c20338336659041ec097c36c08e926f79b378
asuma3399 <fujimoto3399@gmail.com>
2024-08-19 14:32:53 +0900

    ビルドスクリプトの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-export LDFLAGS="-L/home/linuxbrew/.linuxbrew/opt/openssl@3/lib"
-export CPPFLAGS="-I/home/linuxbrew/.linuxbrew/opt/openssl@3/include"
-export PKG_CONFIG_PATH="/home/linuxbrew/.linuxbrew/opt/openssl@3/lib/pkgconfig"
-export LD_LIBRARY_PATH="/home/linuxbrew/.linuxbrew/opt/openssl@3/lib:$LD_LIBRARY_PATH"
 
 bundle install
 bundle exec rake assets:precompile
