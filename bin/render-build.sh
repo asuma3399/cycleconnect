0b46787f138e72f890889faec9e6495f6fcff1f2
asuma3399 <fujimoto3399@gmail.com>
2024-08-19 12:22:46 +0900

    ビルドスクリプトの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# OpenSSLのライブラリパスを設定
+export LD_LIBRARY_PATH=/home/linuxbrew/.linuxbrew/Cellar/openssl@3/3.3.1/lib:$LD_LIBRARY_PATH
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
